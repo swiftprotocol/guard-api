45407d8dacef33571fea3e205d2b7a37c2b3cb3d
Josef Leventon <josef.leventon@gmail.com>
2023-05-17 20:30:32 -0400

    Remove logs

--- src/data.ts
+++ src/data.ts
@@ -2,18 +2,13 @@ import { hdkey } from 'ethereumjs-wallet'
 import EthCrypto from 'eth-crypto'
 
 export async function retrieveData(address: string, key: string): Promise<string> {
-  console.log(`SELECT value FROM key_value WHERE key = '${key}+${address}'`)
   const data = await globalThis.sql
     .query(`SELECT value FROM key_value WHERE key = '${key}+${address}'`)
     .catch((err) => {
       throw Error(err.stack)
     })
 
-  console.log(data)
-
   const { value } = data.rows[0]
-
-  console.log(value)
 
   if (!value || Object.keys(value).length === 0) {
     throw Error('Key does not exist')
