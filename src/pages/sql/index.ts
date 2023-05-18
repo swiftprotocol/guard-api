71abd04db69918fc0ac19ac7ed9ea6ad6d2e2c40
Josef Leventon <josef.leventon@gmail.com>
2023-05-17 20:48:36 -0400

    Log hostname

--- src/pages/sql/index.ts
+++ src/pages/sql/index.ts
@@ -11,6 +11,8 @@ router.post('/', async (req: Request, res: Response): Promise<Response> => {
   const { query, values }: { query: string; values?: any[] } = req.body
   const hostname = req.hostname
 
+  console.log(hostname)
+
   if (!query) return res.status(422).json({ error: 'Missing query.' })
   if (query.includes('$') && (!values || values.length < 1)) return res.status(422).json({ error: 'Missing values.' })
 
